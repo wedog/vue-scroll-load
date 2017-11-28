db37b40e5db35b9718daf3430a477e6cadcd1771
ly_wu <ly_wuyi@163.com>
2017-11-28 18:10:06 +0800

    1. add examples

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -2,20 +2,55 @@ import './scroll.css'
 
 export default{
     name: "VueScrollWrapper",
+    data(){
+        return {
+            upWrapper: false
+        }
+    },
     props: {
         offsetTop: {
             type: String,
             required: false
         }
     },
+    methods: {
+        getDomRect(){
+            return this.$el.getBoundingClientRect();
+        },
+        getScrollHeight(){
+            return this.$el.scrollHeight;
+        },
+        getScrollTop(){
+          return this.$el.scrollTop;
+        },
+        scrollEvent(){
+            let domRect = this.getDomRect();
+            let scrollTop = this.getScrollTop();
+            let scrollHeight = this.getScrollHeight();
+            console.log(domRect)
+            if(domRect && (domRect.height + scrollTop) === scrollHeight){
+                this.upWrapper = true;
+            }else{
+                this.upWrapper = false;
+            }
+        }
+    },
     render(h){
         return h('div', {
+            on: {
+                scroll: this.scrollEvent
+            },
             class: {
                 'vue-scroll-wrapper': true
             },
             style: {
                 top: this.offsetTop || '3rem'
             }
-        }, this.$slots.default)
+        }, [ this.$slots.default, h('div', {
+            class: {
+                'up-wrapper': true,
+                'up-show': this.upWrapper
+            }
+        },[ h('div', '我是有底线的') ]) ])
     }
 }
